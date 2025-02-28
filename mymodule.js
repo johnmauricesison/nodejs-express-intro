2c4045c468aaa676a7f84f26eb249de07bb968a2
John Maurice P. Sison <128139176+johnmauricesison@users.noreply.github.com>
2025-02-28 12:10:32 +0800

    mymodule.js
    Added price format comma function

--- mymodule.js
+++ mymodule.js
@@ -4,20 +4,24 @@ exports.myDatetimeFunction = function () {
     return Date();
 };
 
+exports.NumFormat = function (num) {
+    return num.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+}
+
 exports.InterestValue = function(loanAmount, paymentTerms) {
     let interest = loanAmount * 0.01 * paymentTerms;
-    return interest.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+    return interest
 }
 
 exports.TotalAmount = function(loanAmount, paymentTerms) {
     let interest = loanAmount * 0.01 * paymentTerms;
     let totalamount = loanAmount + interest;
-    return totalamount.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+    return totalamount
 }
 
 exports.Amortization = function(loanAmount, paymentTerms) {
     let interest = loanAmount * 0.01 * paymentTerms;
     let totalamount = loanAmount + interest;
     let amortization = totalamount / paymentTerms;
-    return amortization.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+    return amortization
 }
